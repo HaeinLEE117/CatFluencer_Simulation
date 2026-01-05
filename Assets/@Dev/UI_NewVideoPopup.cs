c8c83952cadcad848386f39ab08891f8915d6171
HaeinLEE117 <heyin0122@korea.ac.kr>
2026-01-06 03:34:43 +0900

    [Feat] 3차 UI Popup 일부 구현

---
+++ Assets/@Dev/UI_NewVideoPopup.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class UI_NewVideoPopup : UI_UGUI, IUI_Popup
+{
+    enum GameObjects
+    {
+
+    }
+
+    enum Buttons
+    {
+        StartButton,
+    }
+
+    enum Texts
+    {
+
+    }
+
+protected override void Awake()
+    {
+        base.Awake();
+
+        BindObjects(typeof(GameObjects));
+        BindButtons(typeof(Buttons));
+        BindTexts(typeof(Texts));
+
+        GetButton((int)Buttons.StartButton).onClick.AddListener(ClosePopup);
+    }
+
+    private void ClosePopup()
+    {
+        UIManager.Instance.ClosePopupUI();
+    }
+}
